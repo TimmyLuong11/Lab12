26fcb2d4b7649b729f893c58161749d4bc3ff031
TimmyLuong11 <Timothy.luong@ou.edu>
2021-04-28 22:45:18 -0500

    Initial commit

---
+++ src/Todo.java
@@ -0,0 +1,30 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-05-02
+OU ID_______________: 113401608
+OU EMAIL____________: timothy.luong@ou.edu
+
+Tasks:
+1. Complete setVisual
+2. Complete DataEntryFrame
+3. Complete mouseClick
+4. Complete signaturePanel
+5. Complete Error
+6. Complete button action
+
+Estimated Time (minutes):
+1. 15
+2. 30
+3. 45
+4. 60
+5. 45
+6. 60
+
+Actual Time (minutes):
+1. 30
+2. 45
+3. 60
+4. 120
+5. 60
+6. 120
+*/}
